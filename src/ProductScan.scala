bf0af657f0d532a2724145eca1f15d095fe1f1b7
dwalend <david@walend.net>
2024-06-30 23:27:57 -0400

    Done for the night, but it's down to a test problem and a clean-up pass.

--- src/ProductScan.scala
+++ src/ProductScan.scala
@@ -30,14 +30,15 @@ case class EnumeratedAttribute(id:AttributeKey,value:String) extends Attribute(i
 
 case class Product(id:ProductId,features:Map[AttributeKey,Attribute]) {
   def name = features(AttributeKey.name)
-  def price = features(AttributeKey.price)
+
+  def price: Double = features.get(AttributeKey.price).collect { case na: NumericAttribute => na }.get.value
 }
 
 //Conditions will look like
 //Condition(AttributeKey.price,case price:NumericAttribute => price.value < 238.9)
 
 //todo can that be Unit?
-case class Condition[T](forKey:AttributeKey,pf:PartialFunction[Attribute,Boolean]) {
+case class Condition(forKey:AttributeKey,pf:PartialFunction[Attribute,Boolean]) {
 
   def test(product:Product):Boolean = {
     //if a product doesn't have a feature then false
@@ -45,34 +46,60 @@ case class Condition[T](forKey:AttributeKey,pf:PartialFunction[Attribute,Boolean
   }
 }
 
-//I'll likely want to specialize Score once I learn more about it, at least what values are in-bounds. Maybe it can be an Int.
-class Score(v:Double) extends AnyVal
+object Condition {
+  def create(keyString:String,pf:PartialFunction[Attribute,Boolean]):Condition = {
+    Condition(AttributeKey(keyString), pf)
+  }
+}
 
-case class Rule(fullScore:Double,conditions:Seq[Condition[?]]) {
-  def findScore(product:Product):Score = {
+case class Rule(fullScore:Double,conditions:Seq[Condition]) {
+  def findScore(product:Product):Double = {
     val total = conditions.map { condition =>
       condition.test(product)
     }.filter(_ == true).size
-    new Score((total * fullScore) / conditions.size)
+    (total * fullScore) / conditions.size
   }
 }
 
 object Rule {
+
+  def apply(string:String):Rule = ???
+
   /**
    *
    * @param rules
    * @param products
    * @return total and average price of all the products that pass the threshold
    */
-  def scanProducts(threshold:Score,rules:Seq[Rule],products:Iterable[Product]):(Double,Double) = ???
+  def scanProducts(rules:Seq[Rule],products:Iterable[Product],threshold:Double = 50.0):(Double,Double) = {
+    val (sum,length) = products.map{ p =>
+        p -> rules.map(r => r.findScore(p)).sum  //todo reread the problem - double-check this
+    }.filter(_._2 > threshold).
+      map(_._1.price). //just need price from here on out
+      foldLeft((0.0,0)){(soFar,price) => ((soFar._1 + price),(soFar._2 + 1))} //gather the total and the length in one pass
+    (sum,sum/length)
+  }
+}
+
+/**
+ * Extensions to support inlining the rules syntax. Seems OK for a prototype but not for a real system.
+ */
+extension (sc: StringContext) {
+  def rule(args: Any*): Rule = Rule(sc.toString)
 }
 
 def main(args:Array[String]):Unit = {
-  val colorKey = AttributeKey("color")
-  val quantityKey = AttributeKey("quantity")
+  val handEncodedRule = Rule(100,Seq(
+    Condition.create("color",{case ea:EnumeratedAttribute => ea.value == "BLUE"}),
+    Condition(AttributeKey.price,{case na:NumericAttribute => na.value < 17.75}),
+    Condition.create("quantity",{case na:NumericAttribute => na.value > 750}),
+  ))
+
+  val blueCheapBulkThing = Product(new ProductId(1),Map(???))
 
 
 
+  val exampleRule = rule"color == BLUE && price < 17.75 && quantity > 750  100"
 }
 
 
